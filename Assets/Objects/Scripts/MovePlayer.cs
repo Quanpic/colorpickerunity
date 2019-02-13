2923d1f73c15b8b6998809bcf9aa483e770df7a2
Quanpic <ishin.daniil@gmail.com>
2019-02-13 14:28:40 +0300

    First commit

---
+++ Assets/Objects/Scripts/MovePlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public Transform player;
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (mousePos.x > 2.6f) mousePos.x = 2.6f;
+        if (mousePos.x < -2.6f) mousePos.x = -2.6f; 
+        player.position = new Vector2(mousePos.x, player.position.y);
+    }
+}
